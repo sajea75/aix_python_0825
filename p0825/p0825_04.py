e5598833222b68bc60c55d52113790fb374ba8fc
park young hun <sajea7@gmail.com>
2026-08-26 10:22:10 +0900

    파이썬수업-print,변수,타입

---
+++ p0825/p0825_04.py
@@ -0,0 +1,53 @@
+# 타입 확인
+a =100
+b =10.1
+c = "안녕"
+# c가 무슨타입?
+# 타입 확인 방법 type(a)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) #몫 2
+print(a%b) # 나머지 0
+print(a**b) #제곱 100000
+print(a+b)
+print(a-b)
+printa(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+      
+
+a = 10   # 숫자형타입 - 정수타입
+b = 10.1 # 숫자형타입 - 실수타입, 소수점
+aa = "안녕" # 문자열 타입
+abc = True # 불타입(bool) -True,False / boolean
+
+# 예약어는 변수로 사용할수 없음.
+# teue = 1
+# p
+
+# 하나만 축력 합니다.
+print("# 하나만 출력 합니다.")
+print("hello pyton programming...!")
+print()
+
+#여려 개를 출력합니다.
+print("# 여러 개를 출력 합니다.")
+print(10, 20, 30, 40, 50)
+print("안녕하세요", "저의", "이름은" "홍길동입니다!")
+print()
+
+# 아무것도 입력하지 않으면 단순하게 줄바꿈합니다.
+print("# 아무것도 출력하지 않습니다.")
+print("---확인 전용선 ---")
+print()
+print()
+print("--- 확인 전용선 ---")
