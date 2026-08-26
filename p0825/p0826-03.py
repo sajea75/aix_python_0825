6faa471a03e739de9340d09071c77a64ee120bd6
park young hun <sajea7@gmail.com>
2026-08-26 14:07:38 +0900

    진수

---
+++ p0825/p0826-03.py
@@ -0,0 +1,58 @@
+# print : 출력
+# print : 입력
+# num =input("숫자를 입력 하세요")
+print("입력숫자 : {}".format(num))
+
+
+# a = inupt("1번째 숫자를 입력하세요.")) # str타입을 int 타입으로 변경
+# b = inuot("2번째 숫자를 입력 하세요.")
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)  #10*10*10 / 10에 3승
+
+# 아이디,패스워드를 입력받아 출력하시오.
+아이디:aaa, 패스워드:1111
+
+a = input("아이디를 입력 하세요")
+b = input("패스워드를 입력하세요")
+print("아이디확인 : {}("aaa"==id)) #true,False
+print("패스워드확인 : {}".format("1111"==pw))
+print("아이디")
+print("아이디:{}, 패스워드:{}".format(a,b))
+
+
+
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b) #10*10*10 / 10에 3승
+
+#a = 10
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b) #10*10*10 /10에3승
+
+num1 = 100
+num2 = 100
+num3 = 100
+print(num1,num2,num3)
+
+# 한줄에 여러변수에 1의 값을 넣는것 가능
+num4=num=5=num6=1
+print(num4,num5,num6)
+
+# 한줄에 여러변수에 여러게 을 넣는 것은 불가
+# a1=1,a2=2 에러
+a1=1
+a2=2
+print(a1,a2)
+
+no1 = 100 #변수선언과 동시에 값전달
+print(10==10) # 같다는 
