35c20531a43109bdff6a4d9a62fcf51a52b9b5bb
park young hun <sajea7@gmail.com>
2026-08-27 12:48:22 +0900

    성적 재시험 표

---
+++ p0827_06.py
@@ -0,0 +1,44 @@
+import random
+# 0-100점  랜덤 숫자 생성
+# 60점 이상 합격
+# 50-59점까지 재시험
+#0-49점까지 불합격
+
+ import
+ 
+
+
+
+
+
+
+
+
+
+# import random
+
+# random_no = random.randint(-2,2)
+# print("랜덤 숫자 : ", random_no)
+
+# # 양수, 음수, 0 판단 및 출력
+# if random_no > 0:
+#     print("양수입니다.") 
+# elif random_no == 0:
+#     print("음수입니다.")
+# else:
+#     print("0입니다.")
+
+
+
+#  # 조건문을 여러개
+# score = 65
+# if score>=90:
+#     print("A")
+# elif score>=80:
+#     print("B")
+# elif score>=70:
+#     print("C")
+# elif score>=60:
+#     print("D")
+# else:
+#     print("F")
