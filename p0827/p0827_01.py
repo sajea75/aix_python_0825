92c1b333cfc9f56e3c1e3b3e4f9503b58637552b
park young hun <sajea7@gmail.com>
2026-08-28 12:03:56 +0900

    전과정 복습

--- p0827/p0827_01.py
+++ p0827/p0827_01.py
@@ -40,3 +40,30 @@ print("번호\t이름\t국어\t영어\t수학\t\과학\t합계\t평균")
 print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{:.2f}".\
       format(no,name,kor,eng,math,sci,total,avg))
 print("-"*60)
+
+
+# 1 박영훈 100 100 100 100
+no =input("번호입력 : ")
+name = input("이름입력 : ")
+kor = int(input("국어점수 입력>> "))
+eng = int(input("영어점수 입력>> "))
+math = int(input("수학점수 입력>> "))
+sci = int(input("과학점수 입력>> "))
+total = kor+eng+math+sci
+avg = total/4
+
+print("-"*60)
+
+print("번호\t이름\t국어\t영어\t수학\t\과학\t합계\t평균")
+print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{:.2f}".\
+      format(no,name,kor,eng,math,sci,total,avg))
+
+# 2 이경화 100 90 80 70
+no2 =input("번호입력: ")
+name2 = input("이름입력 ")
+kor2 = int(input("국어점수 압력>> "))
+eng2 = int(input("영어점수 압력>> ")) 
+math2 = int(input("수학점수 입력>> "))
+sci2 = int(input("과학점수 입력>> "))
+total2 = kor+eng+math+sci
+avg2 = total/4
---
+++ p0828_05.py
@@ -0,0 +1,18 @@
+a = "11"
+print(type(int(a)))
+
+b = 1.1
+print(int(b))
+print(float(b))
+
+c =10
+d =3
+e =10//3 
+print(type(e))
+
+f = 5
+if f&2==0:print("짝수")
+else:print("홀수")
+
+resuit = "짝수" if f%2==0 else "홀수"
+print(resuit)
