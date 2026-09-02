a01f7546b66659e6f7c10dd174397a9701e6b328
park young hun <sajea7@gmail.com>
2026-09-02 18:09:21 +0900

    오후수업

---
+++ p0902/p0902_04.py
@@ -0,0 +1,35 @@
+
+# 함수사용이유 : 긴 구문의 반복적인 명령어르르 줄일수 있음
+# 코드를 간결하게 
+def stu_puint():
+    for s in stu:
+        print("{},{},{},{},{}".format(*s))
+
+stu = [
+    [1,"홍길동",100,100,100],
+    [2,"유관순",100,100,100],
+    [3,"이순신",100,100,100]
+]
+
+while True:
+    print("1. 학생성적입력")
+
+    print("2. 학생성적출력")
+    print("3. 학생성적검색")
+
+    choice = int(input("원하는 번호를 입력하세요.>> "))
+    if choice == 1:
+        name = input("이름을 입력하세요.")
+        # 학생전체출력
+        # # for s in stu:
+        # #     print("{},{},{},{},{}".format(*s))
+        # stu_puint()
+    elif choice == 2:
+        # 학생출력하는 구문 
+        print("번호\t이름\t국어\t영어\t수학\t합계\t평균")
+        # 학생전체출력
+        stu_puint()
+        # for s in stu:
+        # print("{},{},{},{},{}".format(*s))
+    else:
+        name = input("이름을 입력하세요.")
